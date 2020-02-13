f11bb750dfbe1c3638b40fb8290f7228f2b524bc
Timo <dev@timot.se>
2020-02-13 07:35:57 +0100

    rename for clarity

--- system/src/main.rs
+++ system/src/main.rs
@@ -8,22 +8,22 @@ use wasmtime::{Module as WasmModule, *};
 type Server = HashMap<String, Module>;
 
 #[derive(Clone, Default)]
-struct Servers {
-    list: Arc<RwLock<Vec<Server>>>,
+struct System {
+    servers: Arc<RwLock<Vec<Server>>>,
 }
 
-impl Servers {
+impl System {
     fn add(&self, server: Server) -> Result<(), ()> {
-        self.list.write().map(|mut list| list.push(server)).map_err(|_| ())
+        self.servers.write().map(|mut list| list.push(server)).map_err(|_| ())
     }
 
     fn run(&self, module: &str, function: &str, params: &[Val]) -> Result<Box<[Val]>, Trap> {
-        self.list
+        self.servers
             .read()
             .map_err(|_| Trap::new("Could not read rwlock"))?
             .iter()
             .find_map(|server| server.get(module))
-            .ok_or_else(|| Trap::new("No server found with module loaded"))?
+            .ok_or_else(|| Trap::new("No instance of module found"))?
             .run(function, params)
     }
 }
@@ -31,13 +31,13 @@ impl Servers {
 struct CallRoute {
     module: String,
     function: String,
-    servers: Servers,
+    system: System,
 }
 
 impl wasmtime::Callable for CallRoute {
     fn call(&self, params: &[Val], results: &mut [Val]) -> Result<(), Trap> {
         println!("Calling {}::{}", self.module, self.function);
-        let result = self.servers.run(&self.module, &self.function, params)?;
+        let result = self.system.run(&self.module, &self.function, params)?;
 
         for (idx, result) in result.into_iter().enumerate() {
             results[idx] = result.clone();
@@ -66,52 +66,52 @@ impl Module {
 }
 
 fn main() {
-    let servers = Servers::default();
+    let system = System::default();
 
-    servers
+    system
         .add(create_server(
             &[(
                 "https://repository.timot.se/test1",
                 "./target/wasm32-unknown-unknown/release/test1.wasm",
             )],
-            &servers,
+            &system,
         ))
         .unwrap();
 
-    servers
+    system
         .add(create_server(
             &[(
                 "https://repository.timot.se/test2",
                 "./target/wasm32-unknown-unknown/release/test2.wasm",
             )],
-            &servers,
+            &system,
         ))
         .unwrap();
 
-    servers
-        .add(create_server(&[("https://repository.timot.se/test3", "./test3/main.wasm")], &servers))
+    system
+        .add(create_server(&[("https://repository.timot.se/test3", "./test3/main.wasm")], &system))
         .unwrap();
 
     println!("Running https://repository.timot.se/test1::return_double_arg");
     println!(
         "{:?}",
-        servers
+        system
             .run("https://repository.timot.se/test1", "return_double_arg", &[111.into()])
             .unwrap()
     );
     println!("\nRunning https://repository.timot.se/test2::return_arg");
     println!(
         "{:?}",
-        servers.run("https://repository.timot.se/test2", "return_arg", &[222.into()]).unwrap()
+        system.run("https://repository.timot.se/test2", "return_arg", &[222.into()]).unwrap()
     );
     println!("\nRunning https://repository.timot.se/test3::return_arg");
     println!(
         "{:?}",
-        servers.run("https://repository.timot.se/test3", "return_arg", &[333.into()]).unwrap()
+        system.run("https://repository.timot.se/test3", "return_arg", &[333.into()]).unwrap()
     );
 }
 
-fn create_server(binaries: &[(&str, &str)], servers: &Servers) -> Server {
+fn create_server(binaries: &[(&str, &str)], system: &System) -> Server {
     binaries
         .into_iter()
         .map(|(name, path)| {
@@ -120,7 +120,7 @@ fn create_server(binaries: &[(&str, &str)], servers: &Servers) -> Server {
             let module = WasmModule::new(&store, &bin).expect("wasm module");
 
             (name.to_string(), Module {
-                instance: Instance::new(&store, &module, &map_imports(module.imports(), servers))
+                instance: Instance::new(&store, &module, &map_imports(module.imports(), system))
                     .unwrap(),
                 exports: map_exports(module.exports()).collect(),
             })
@@ -128,12 +128,12 @@ fn create_server(binaries: &[(&str, &str)], servers: &Servers) -> Server {
         .collect()
 }
 
-fn map_imports(imports: &[ImportType], servers: &Servers) -> Vec<Extern> {
+fn map_imports(imports: &[ImportType], system: &System) -> Vec<Extern> {
     imports
         .iter()
         .filter_map(|import| {
             let route = CallRoute {
-                servers: servers.clone(),
+                system: system.clone(),
                 module: import.module().into(),
                 function: import.name().into(),
             };
